fb6767f6a46825276b4bdf0a97e93df1ed9ac3db
Mark Gross <mark.gross2001@gmail.com>
2018-11-04 17:18:54 -0800

    Add gitignore; add decryption to affine

--- affine.c
+++ affine.c
@@ -12,6 +12,16 @@ void print_usage(char* arg0)
 		printf("\t-b, --key-b\t\tUse specified key B\n");
 }
 
+int mod_inverse(int a, int m)
+{
+	// Since our modulus is only 26, we don't have to do this very efficiently
+	a = a % m;
+	for (int i = 1; i < m; i++)
+		if ((a * i) % m == 1)
+			return i;
+	return 0; // No inverse
+}
+
 int main(int argc, char* argv[])
 {
 	if (argc < 3)
@@ -20,7 +30,7 @@ int main(int argc, char* argv[])
 		return 1;
 	}
 
-	int key_a, key_b;
+	int key_a, key_b, decrypt = 0;
 
 	if (argc == 3)
 	{
@@ -40,30 +50,61 @@ int main(int argc, char* argv[])
 			key_a = atoi(argv[i+1]);
 		else if (!strcmp(argv[i], "-b") || !strcmp(argv[i], "--key-b"))
 			key_b = atoi(argv[i+1]);
+		else if (!strcmp(argv[i], "-d") || !strcmp(argv[i], "--decrypt"))
+			decrypt = 1;
 	}
 
 	int i, c;
-	for (i = 0; (c = getchar()) != EOF; i++)
+	if (!decrypt)
 	{
-		int alphabet_pos;
-		int is_capital = 0;
-		if (c >= 'a' && c <= 'z')
-			alphabet_pos = c - 'a';
-		else if (c >= 'A' && c <= 'Z')
+		for (i = 0; (c = getchar()) != EOF; i++)
 		{
-			is_capital = 1;
-			alphabet_pos = c - 'A';
-		}
-		else // character is not a letter; output it as-is and move on
-		{
-			putchar(c);
-			continue;
-		}
+			int alphabet_pos, is_capital = 0;
+			if (c >= 'a' && c <= 'z')
+				alphabet_pos = c - 'a';
+			else if (c >= 'A' && c <= 'Z')
+			{
+				is_capital = 1;
+				alphabet_pos = c - 'A';
+			}
+			else // character is not a letter; output it as-is and move on
+			{
+				putchar(c);
+				continue;
+			}
 
-		char enciphered_char_pos = (key_a * alphabet_pos + key_b) % 26;
-		if (is_capital)
-			putchar(enciphered_char_pos + 'A');
-		else
-			putchar(enciphered_char_pos + 'a');
+			char enciphered_char_pos = (key_a * alphabet_pos + key_b) % 26;
+			if (is_capital)
+				putchar(enciphered_char_pos + 'A');
+			else
+				putchar(enciphered_char_pos + 'a');
+		}
+	}
+	else
+	{
+		for (i = 0; (c = getchar()) != EOF; i++)
+		{
+			int alphabet_pos, is_capital = 0;
+			if (c >= 'a' && c <= 'z')
+				alphabet_pos = c - 'a';
+			else if (c >= 'A' && c <= 'Z')
+			{
+				is_capital = 1;
+				alphabet_pos = c - 'A';
+			}
+			else // character is not a letter; output it as-is and move on
+			{
+				putchar(c);
+				continue;
+			}
+
+			char deciphered_char_pos = (mod_inverse(key_a, 26) * (alphabet_pos - key_b)) % 26;
+			if (deciphered_char_pos < 0) // why does C make me do this :(
+				deciphered_char_pos = 26 + deciphered_char_pos;
+			if (is_capital)
+				putchar(deciphered_char_pos + 'A');
+			else
+				putchar(deciphered_char_pos + 'a');
+		}
 	}
 }
